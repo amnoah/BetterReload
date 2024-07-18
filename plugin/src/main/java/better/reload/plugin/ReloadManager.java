bebf7d929c67e0d38a42d6495d505781c435233b
Noah <79341053+amnoah@users.noreply.github.com>
2024-07-17 22:27:40 -0400

    Centralize Reloading

---
+++ plugin/src/main/java/better/reload/plugin/ReloadManager.java
@@ -0,0 +1,127 @@
+package better.reload.plugin;
+
+import better.reload.api.ReloadEvent;
+import better.reload.plugin.util.Configuration;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.RegisteredListener;
+
+/**
+ * This class provides the basic functionality behind the reloading process.
+ * It has been separated from the reload command itself so other plugins can also call these methods as deemed necessary.
+ */
+public class ReloadManager {
+
+    /**
+     * Initiate a reload for all plugins using the console as the CommandSender.
+     */
+    public static void reload() {
+        reload(Bukkit.getConsoleSender());
+    }
+
+    /**
+     * Initiate a reload for all plugins using a given CommandSender.
+     */
+    public static void reload(CommandSender commandSender) {
+        ReloadEvent event = new ReloadEvent(commandSender);
+
+        /*
+         * We could use Bukkit.getPluginManager().callEvent(), but I'd prefer to use my own error logging system.
+         * So, we manually pass this event to all applicable listeners.
+         */
+        for (RegisteredListener listener : ReloadEvent.getHandlerList().getRegisteredListeners()) {
+            try {
+                listener.callEvent(event);
+            } catch (Exception e) {
+                BetterReload.PLUGIN.getLogger().warning("Error reloading " + listener.getPlugin().getName() + "!");
+            }
+        }
+    }
+
+    /**
+     * Initiate a reload for the given plugin using the console as the CommandSender.
+     * Return whether the plugin has a listener for the event.
+     */
+    public static boolean reload(Plugin plugin) {
+        return reload(Bukkit.getConsoleSender(), plugin);
+    }
+
+    /**
+     * Initiate a reload for the given plugin using a given CommandSender.
+     * Return whether the plugin has a listener for the event.
+     */
+    public static boolean reload(CommandSender commandSender, Plugin plugin) {
+        if (plugin == null) return false;
+        boolean reloaded = false;
+        ReloadEvent event = new ReloadEvent(commandSender);
+
+        for (RegisteredListener listener : ReloadEvent.getHandlerList().getRegisteredListeners()) {
+            try {
+                if (listener.getPlugin() == plugin) {
+                    reloaded = true;
+                    listener.callEvent(event);
+                }
+            } catch (Exception e) {
+                BetterReload.PLUGIN.getLogger().warning("Error reloading " + plugin.getName() + "!");
+            }
+        }
+
+        return reloaded;
+    }
+
+    /**
+     * This represents the stage at which commands should be performed.
+     * This can either be before the event was sent (pre) or after the event was sent (post).
+     */
+    public enum CommandStage {
+        PRE_RELOAD("pre:"),
+        POST_RELOAD("post:");
+
+        private String configName;
+
+        CommandStage(String configName) {
+            this.configName = configName;
+        }
+
+        public String getConfigName() {
+            return configName;
+        }
+    }
+
+    /**
+     * Run applicable commands from the BetterReload config according to the CommandStage and CommandSender.
+     */
+    public static void runCommands(CommandStage stage, CommandSender commandSender) {
+        if (commandSender instanceof Player) {
+            for (String command : Configuration.PLAYER_COMMANDS) {
+                String[] commandElements = command.split(":", 3);
+                if (commandElements.length <= 2) continue;
+                if (!commandElements[0].toLowerCase().startsWith(stage.getConfigName())) continue;
+
+                switch (commandElements[1].toLowerCase()) {
+                    case "player":
+                        Bukkit.dispatchCommand(commandSender, commandElements[2].replace("%ReloadCommandSender%", commandSender.getName()));
+                        break;
+                    case "console":
+                        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), commandElements[2]);
+                        break;
+                    default:
+                        BetterReload.PLUGIN.getLogger().warning("Unsupported player command mode!");
+                        BetterReload.PLUGIN.getLogger().warning("Player command mode: " + commandElements[1]);
+                }
+            }
+        } else if (commandSender instanceof ConsoleCommandSender) {
+            for (String command : Configuration.CONSOLE_COMMANDS) {
+                if (!command.toLowerCase().startsWith(stage.getConfigName())) continue;
+                if (command.length() == stage.getConfigName().length()) continue;
+                Bukkit.dispatchCommand(commandSender, command.substring(stage.getConfigName().length()));
+            }
+        } else {
+            BetterReload.PLUGIN.getLogger().warning("Unsupported CommandSender in runCommands method!");
+            BetterReload.PLUGIN.getLogger().warning("CommandSender type: " + commandSender.getClass().getName());
+        }
+    }
+}
--- plugin/src/main/java/better/reload/plugin/command/ReloadCommand.java
+++ plugin/src/main/java/better/reload/plugin/command/ReloadCommand.java
@@ -2,6 +2,7 @@ package better.reload.plugin.command;
 
 import better.reload.api.ReloadEvent;
 import better.reload.plugin.BetterReload;
+import better.reload.plugin.ReloadManager;
 import better.reload.plugin.util.ChatColor;
 import better.reload.plugin.util.Configuration;
 import org.bukkit.Bukkit;
@@ -10,7 +11,6 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventException;
 import org.bukkit.plugin.RegisteredListener;
 
 import java.util.ArrayList;
@@ -31,15 +31,9 @@ public class ReloadCommand implements CommandExecutor, TabExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         final long startReload = System.currentTimeMillis();
-        String senderName = commandSender instanceof Player ? commandSender.getName() : "";
 
         String start = Configuration.START_RELOAD_MESSAGE;
         if (!start.isEmpty()) sendMessage(commandSender, start);
-
-        // Pre-Event Commands.
-        runCommands("pre:", commandSender);
-
-        ReloadEvent event = new ReloadEvent(commandSender);
 
         // If there's additional inputs attempt a reload for the specific inputted plugins.
         if (strings.length > 0) {
@@ -55,61 +49,20 @@ public class ReloadCommand implements CommandExecutor, TabExecutor {
              * plugin1 twice.
              */
             for (String pluginName : strings) {
-                boolean reloaded = false;
-
-                for (RegisteredListener listener : ReloadEvent.getHandlerList().getRegisteredListeners()) {
-                    if (!pluginName.equalsIgnoreCase(listener.getPlugin().getName())) continue;
-
-                    try {
-                        listener.callEvent(event);
-                        reloaded = true;
-                    } catch (EventException ignored) {
-                        BetterReload.PLUGIN.getLogger().warning("Error reloading " + pluginName.toLowerCase() + "!");
-                    }
-                }
-
+                boolean reloaded = ReloadManager.reload(commandSender, Bukkit.getPluginManager().getPlugin(pluginName));
                 if (!reloaded) sendMessage(commandSender, Configuration.PLUGIN_NOT_SUPPORTED_MESSAGE.replaceAll("%input%", pluginName));
             }
         // If there's no additional input then call an event for all plugins.
-        } else Bukkit.getPluginManager().callEvent(event);
-
-        // Post-Event Commands.
-        runCommands("post:", commandSender);
+        } else {
+            ReloadManager.runCommands(ReloadManager.CommandStage.PRE_RELOAD, commandSender);
+            ReloadManager.reload(commandSender);
+            ReloadManager.runCommands(ReloadManager.CommandStage.POST_RELOAD, commandSender);
+        }
 
         String end = Configuration.END_RELOAD_MESSAGE.replaceAll("%ms%",String.valueOf(System.currentTimeMillis() - startReload));
         if (!end.isEmpty()) sendMessage(commandSender, end);
 
         return true;
-    }
-
-    /**
-     * Runs PRE- / POST-commands as provided in the configuration file.
-     */
-    private static void runCommands(String stage, CommandSender commandSender) {
-        if (commandSender instanceof Player) {
-            for (String command : Configuration.PLAYER_COMMANDS) {
-                if (!command.toLowerCase().startsWith(stage)) continue;
-                if (command.length() == stage.length()) continue;
-
-                String[] commandElements = command.substring(stage.length()).split(":", 2);
-                if (commandElements.length <= 1) continue;
-
-                switch (commandElements[0].toLowerCase()) {
-                    case "player":
-                        Bukkit.dispatchCommand(commandSender, commandElements[1].replaceAll("%ReloadCommandSender%", commandSender.getName()));
-                        break;
-                    case "console":
-                        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), commandElements[1]);
-                        break;
-                }
-            }
-        } else {
-            for (String command : Configuration.CONSOLE_COMMANDS) {
-                if (!command.toLowerCase().startsWith(stage)) continue;
-                if (command.length() == stage.length()) continue;
-                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command.substring(stage.length()));
-            }
-        }
     }
 
     /**
@@ -136,7 +89,7 @@ public class ReloadCommand implements CommandExecutor, TabExecutor {
     /**
      * This void reduces redundancy in the process of sending messages.
      */
-    public void sendMessage(CommandSender commandSender, String string) {
+    private void sendMessage(CommandSender commandSender, String string) {
         if (commandSender instanceof Player) {
             commandSender.sendMessage(ChatColor.translateColorCodes(string));
             if (Configuration.LOG_MESSAGES) BetterReload.PLUGIN.getLogger().info(ChatColor.stripColorCodes(string));
