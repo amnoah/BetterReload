7fbce8260c71d5eddf94da275acf4deb6d6f9523
Noah <79341053+amnoah@users.noreply.github.com>
2024-07-22 23:51:40 -0400

    Add custom bStats metrics

--- plugin/src/main/java/better/reload/plugin/BetterReload.java
+++ plugin/src/main/java/better/reload/plugin/BetterReload.java
@@ -1,13 +1,20 @@
 package better.reload.plugin;
 
+import better.reload.api.ReloadEvent;
 import better.reload.plugin.command.ReloadCommand;
 import better.reload.plugin.listener.PreCommandProcessingListener;
 import better.reload.plugin.listener.ReloadListener;
 import better.reload.plugin.listener.TabCompleteListener;
 import better.reload.plugin.util.Configuration;
 import org.bstats.bukkit.Metrics;
+import org.bstats.charts.SimplePie;
 import org.bukkit.event.HandlerList;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.RegisteredListener;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This is the main class for the plugin. This class handles the loading and disabling of all the listeners and bStats.
@@ -40,8 +47,6 @@ public final class BetterReload extends JavaPlugin {
     public void onEnable() {
         PLUGIN = this;
 
-        metrics = new Metrics(this, B_STATS_ID);
-
         Configuration.reload();
 
         getCommand("reload").setExecutor(reloadCommand);
@@ -57,6 +62,32 @@ public final class BetterReload extends JavaPlugin {
 
         getServer().getPluginManager().registerEvents(new PreCommandProcessingListener(), this);
         getServer().getPluginManager().registerEvents(new ReloadListener(), this);
+
+        // Start our metrics.
+
+        metrics = new Metrics(this, B_STATS_ID);
+
+        metrics.addCustomChart(new SimplePie("registered_listeners", () ->
+                String.valueOf(ReloadEvent.getHandlerList().getRegisteredListeners().length)
+        ));
+
+        metrics.addCustomChart(new SimplePie("supported_plugins", () -> {
+            List<Plugin> plugins = new ArrayList<>();
+            for (RegisteredListener listener : ReloadEvent.getHandlerList().getRegisteredListeners()) {
+                if (plugins.contains(listener.getPlugin())) continue;
+                plugins.add(listener.getPlugin());
+            }
+
+            return String.valueOf(plugins.size());
+        }));
+
+        metrics.addCustomChart(new SimplePie("console_commands", () ->
+                String.valueOf(!Configuration.CONSOLE_COMMANDS.isEmpty())
+        ));
+
+        metrics.addCustomChart(new SimplePie("player_commands", () ->
+                String.valueOf(!Configuration.PLAYER_COMMANDS.isEmpty())
+        ));
     }
 
     @Override
